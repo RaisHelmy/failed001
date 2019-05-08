bc9e53c4a43dc4d87e6e7727fc5eb4231814b6bb
RaisHelmy <raishelmy@github.com>
2019-05-08 13:27:03 +0800

    update
    clone n sources from here
    
    https://gist.github.com/ykyuen?page=2

---
+++ api/post_full_name.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "net/http"
+
+  "github.com/labstack/echo"
+
+  "gitlab.com/ykyuen/handling-http-request-in-go-echo-example-1/model"
+)
+
+func PostFullName(c echo.Context) error {
+  // Bind the input data to ExampleRequest
+  exampleRequest := new(model.ExampleRequest)
+  if err := c.Bind(exampleRequest); err != nil {
+    return err
+  }
+
+  // Manipulate the input data
+  greeting := exampleRequest.FirstName + " " + exampleRequest.LastName
+
+  // Pretty print the json []byte
+  var resp bytes.Buffer
+  var b = []byte(
+    fmt.Sprintf(`{
+      "first_name": %q,
+      "last_name": %q,
+      "msg": "Hello %s"
+    }`, exampleRequest.FirstName, exampleRequest.LastName, greeting),
+  )
+  err := json.Indent(&resp, b, "", "  ")
+  if err != nil {
+    return err
+  }
+
+  // Return the json to the client
+  return c.JSONBlob(
+    http.StatusOK,
+    []byte(
+      fmt.Sprintf("%s", resp.Bytes()),
+    ),
+  )
+}
