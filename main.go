a8b0ee1a4974ae3a4c44ebe8cf80cea93891f2a7
RaisHelmy <raishelmy@github.com>
2019-05-08 13:03:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
   "net/http"
+
   "github.com/labstack/echo"
 )
 
 func main() {
   e := echo.New()
+
   e.GET("/", func(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, World!")
   })
+
+  e.GET("/json", func(c echo.Context) error {
+    return c.JSONBlob(
+      http.StatusOK,
+      []byte(`{ "id": "1", "msg": "Hello, Boatswain!" }`),
+    )
+  })
+
   e.Logger.Fatal(e.Start(":1323"))
 }
